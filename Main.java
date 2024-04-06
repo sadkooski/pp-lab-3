b68e0693dfcfec1737bf69ce58586822041abbb5
Oskar Sadkowski <sadkooski@gmail.com>
2024-04-06 15:53:34 +0200

    zad1

---
+++ Main.java
@@ -0,0 +1,38 @@
+
+class Point {
+    
+    private double x;
+    private double y;
+
+    public Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+}
+
+
+public class Main {
+    public static void main(String[] args) {
+        Point point = new Point(2.5, 4.0);
+
+        System.out.println("Współrzędne punktu:");
+        System.out.println("x = " + point.getX());
+        System.out.println("y = " + point.getY());
+    } 
+}
