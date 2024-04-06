7628caf6d6596abb8ab1b036c1d0344b27f09a8d
Oskar Sadkowski <sadkooski@gmail.com>
2024-04-06 18:50:15 +0200

    poprawki zad1

--- Main.java
+++ Main.java
@@ -1,32 +1,4 @@
-
-class Point {
-    
-    private double x;
-    private double y;
-
-    public Point(double x, double y) {
-        this.x = x;
-        this.y = y;
-    }
-
-    public double getX() {
-        return x;
-    }
-
-    public double getY() {
-        return y;
-    }
-
-    public void setX(double x) {
-        this.x = x;
-    }
-
-    public void setY(double y) {
-        this.y = y;
-    }
-}
-
-
+import geometry.Point;
 public class Main {
     public static void main(String[] args) {
         Point point = new Point(2.5, 4.0);
